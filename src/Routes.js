b07360ae8df3b44b0f014e39650be955a6da59e6
Edson Rodrigues <edinho_rodrigues@hotmail.com>
2020-02-05 20:43:31 -0300

    salvando avatar

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,11 +8,20 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.get('/', (req, res) => {
+  return res.json('Edinho Rodrigues');
+})
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
