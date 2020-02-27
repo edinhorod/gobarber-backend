e744629b28df2326dbd3b414938d666173652e95
Edinho Rodrigues <edinho_rodrigues@hotmail.com>
2020-02-27 08:37:22 -0300

    Tratamento de exceções

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import path from 'path';
-import routes from './routes';
+import * as Sentry from '@sentry/node';
 import './database';
+import 'express-async-errors';
+import Youth from 'youch';
+import sentryConfig from './config/sentry';
+import routes from './Routes';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandlers();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +29,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandlers() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youth(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
