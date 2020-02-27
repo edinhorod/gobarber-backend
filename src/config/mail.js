58c1ad015f6547579b7e6131a65749b3f91c6706
Edinho Rodrigues <edinho_rodrigues@hotmail.com>
2020-02-27 09:16:54 -0300

    Variáveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,12 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '18c5ddfe0bb4a0',
-    pass: 'c8bf83c3c9bb14',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+    // user: '18c5ddfe0bb4a0',
+    // pass: 'c8bf83c3c9bb14',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://389cf8cf58724e8481802652aac0eaa2@sentry.io/3051011',
+  dsn: process.env.SENTRY_DSN,
 };
